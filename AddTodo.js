dcf3faf41e7059799b1c15d17bb49f726f2dea2b
Mihail <94056256+Lomer1@users.noreply.github.com>
2022-08-18 15:27:13 +0300

    Add files via upload

---
+++ AddTodo.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {View,StyleSheet,TextInput,Button} from 'react-native'
+export const AddTodo = ({onSubmit}) => {
+    const pressHandler=() => {
+       
+    }
+    return(
+        <View style ={styles.block}>
+            <TextInput style={styles.input}/>
+            <Button title='add' onPress={pressHandler}/>
+        </View>
+    )
+}
+const styles= StyleSheet.create({
+ block:{
+    
+
+  
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems: 'center'
+          },
+              
+                   input:{
+            width:'70%',
+            padding: 10 ,
+            borderStyle: 'solid',
+            borderBottomWidth:2,
+            borderBottomColor:'#3949ab',
+          },
+        conteiner: {
+            
+        }
+        });
+        
+ 
+
+
+
+
---
+++ Support.js
@@ -0,0 +1,9 @@
+import React from "react" ;
+const Support = () => {
+return (
+<div className= "support" >
+<h1>GeeksforGeeks Support us</h1>
+</div>
+);
+};
+export default Support;  
